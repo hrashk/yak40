3e59a8a5c13c711df9f0cdcd5a5cfcb56c46a1ba
Vaagn Khachatryan <vaagn.khachatryan@gmail.com>
2022-10-09 18:22:53 +0200

    add speed lights tests

--- src/main/java/ya/praktikum/sprint3/SpeedLights.java
+++ src/main/java/ya/praktikum/sprint3/SpeedLights.java
@@ -1,4 +1,21 @@
 package ya.praktikum.sprint3;
 
 public class SpeedLights {
+    private final int speedLimit;
+
+    public SpeedLights(int speedLimit) {
+        this.speedLimit = speedLimit;
+    }
+
+    public String showLights(int speed) {
+        if (speed < 0)
+            throw new IllegalArgumentException("speed must not be negative " + speed);
+
+        if (speed < speedLimit)
+            return "green";
+        else if (speed < speedLimit + 20)
+            return "yellow";
+        else
+            return "red";
+    }
 }
--- src/test/java/ya/praktikum/sprint3/SpeedLightsTest.java
+++ src/test/java/ya/praktikum/sprint3/SpeedLightsTest.java
@@ -1,7 +1,31 @@
 package ya.praktikum.sprint3;
 
-import static org.junit.Assert.*;
+import org.junit.Before;
+import org.junit.Test;
+
+import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.assertThrows;
 
 public class SpeedLightsTest {
+    SpeedLights cityLights;
 
+    @Before public void constructLights() {
+        cityLights = new SpeedLights(60);
+    }
+
+    @Test public void showLightNotSpeedingGreen() {
+        assertEquals("green", cityLights.showLights(55));
+    }
+
+    @Test public void showLightSpeedingLessThan20Yellow() {
+        assertEquals("yellow", cityLights.showLights(70));
+    }
+
+    @Test public void showLightSpeedingGreaterThan20Red() {
+        assertEquals("red", cityLights.showLights(81));
+    }
+
+    @Test public void showLightsNegativeThrowsException() {
+        assertThrows(IllegalArgumentException.class, () -> cityLights.showLights(-10));
+    }
 }
